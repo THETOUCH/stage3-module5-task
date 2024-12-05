0284962bc724016717715938f7c1bc44266e255c
THETOUCH <super.ilgiznex@gmail.com>
2024-12-05 23:59:13 +0600

    12345

--- module-web/src/main/java/com/mjc/school/controller/SwaggerConfig.java
+++ module-web/src/main/java/com/mjc/school/controller/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.mjc.school.contoller"))
+                .apis(RequestHandlerSelectors.basePackage("com.mjc.school.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
